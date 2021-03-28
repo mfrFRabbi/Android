7f677eaf0d82ad2c8710d92fc8fca0040e2431ca
mfrfrabbi <mdfazlerabbi.diu@gmail.com>
2021-03-29 01:01:39 +0600

    2nd initial commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView setText;
+    private Button btn,imAndTextBtn;
+    private ImageButton imBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setText = findViewById(R.id.textId);
+        btn = findViewById(R.id.buttonId);
+        imBtn = findViewById(R.id.imageButtonId);
+
+        btn.setOnClickListener(this);
+
+       imBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.buttonId)
+            setText.setText("Welcome!! Button");
+        if(v.getId() == R.id.imageButtonId)
+            setText.setText("Welcome!! imageButton");
     }
 }
