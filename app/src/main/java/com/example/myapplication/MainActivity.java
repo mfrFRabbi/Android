484e3638662fd8accdd56321275d92fff73250cc
mfrfrabbi <mdfazlerabbi.diu@gmail.com>
2021-03-29 15:34:23 +0600

    7th editText

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,46 +8,50 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView setText;
-    private Button btn,imAndTextBtn;
-    private ImageButton imBtn;
+private EditText one,two;
+private Button plus,minus;
+private TextView setText;
+double a,b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setText = findViewById(R.id.textId);
-        btn = findViewById(R.id.buttonId);
-        imBtn = findViewById(R.id.imageButtonId);
+        one = findViewById(R.id.firstText);
+        two = findViewById(R.id.secondText);
+        plus = findViewById(R.id.buttonId);
+        minus = findViewById(R.id.buttonId2);
+        setText = findViewById(R.id.resId);
 
-
- /*    btn.setOnClickListener(new Listener());
-       imBtn.setOnClickListener(new Listener());*/
     }
-/*
-    class Listener implements View.OnClickListener{
-        @Override
-        public void onClick(View v) {
-            if(v.getId() == R.id.buttonId)
-                setText.setText("Welcome!! Button");
-            else if(v.getId() == R.id.imageButtonId)
-                setText.setText("Welcome!! imageButton");
-        }
-    }
- */
 
     public void setListener(View view) {
-        if(view.getId() == R.id.buttonId)
-            Log.d("tag","Message");
-        else if(view.getId() == R.id.imageButtonId) {
-            Toast toast = Toast.makeText(this,"Welcome!! imageButton",Toast.LENGTH_SHORT);
-            toast.setGravity(Gravity.CENTER,0,0);
+
+
+        try {
+            a = Double.parseDouble(one.getText().toString());
+            b = Double.parseDouble(one.getText().toString());
+            if(view.getId() == R.id.buttonId){
+                double res;
+                res = a+b;
+                setText.setText(String.valueOf(res));
+            }
+            else if(view.getId() == R.id.buttonId2) {
+                double res;
+                res = a-b;
+                setText.setText(String.valueOf(res));
+            }
+        }catch (Exception e){
+            Toast toast = Toast.makeText(this,"Please Enter Number",Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER_VERTICAL|Gravity.START ,0,90);
             toast.show();
         }
+
     }
 }
