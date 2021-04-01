ecbe89d16095dbe4a888c2723d3b59058e0f250d
mfrfrabbi <mdfazlerabbi.diu@gmail.com>
2021-04-01 13:35:49 +0600

    17th TimePickerDialog

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,45 +3,63 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private TextView textView;
-    private Button button;
-    private DatePickerDialog datePickerDialog;
+private Button setTime,timeDialog;
+private TimePicker timePicker;
+private TextView timeText,reviewTimeText,timeTextTwo;
+
+private TimePickerDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTime = findViewById(R.id.showTimeId);
+        timeDialog = findViewById(R.id.dialog_buttonId);
+        timePicker = findViewById(R.id.timePickerId);
 
-        textView = findViewById(R.id.text_viewId);
-        button = findViewById(R.id.buttonId);
-        button.setOnClickListener(this);
+        timeText = findViewById(R.id.showTimeTextId);
+        timeTextTwo = findViewById(R.id.timeTextId);
+
+        timePicker.setIs24HourView(true);
+        setTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String time = timePicker.getCurrentHour() +":"+timePicker.getCurrentMinute();
+                timeText.setText(time);
+            }
+        });
+
+        timeDialog.setOnClickListener(this);
+
 
 
     }
 
     @Override
     public void onClick(View v) {
-        DatePicker datePicker = new DatePicker(this);
-        int currentDay = datePicker.getDayOfMonth();
-        int currentMonth = datePicker.getMonth() + 1;
-        int currentYear = datePicker.getYear();
+        TimePicker timePicker = new TimePicker(this);
 
-        datePickerDialog = new DatePickerDialog(this,
-                new DatePickerDialog.OnDateSetListener() {
+        dialog = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
                     @Override
-                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                        textView.setText(dayOfMonth + " /" + (month + 1) + " /" + year);
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        String time = hourOfDay +":"+minute;
+                        timeTextTwo.setText(time);
                     }
                 },
-                currentYear,currentMonth,currentDay);
-        datePickerDialog.show();
+                timePicker.getCurrentHour(),
+                timePicker.getCurrentMinute(),true);
+        dialog.show();
+
     }
 }
