e04f627d9815564dc0f9fa0dd59b26557b81f8ae
mfrfrabbi <mdfazlerabbi.diu@gmail.com>
2021-04-01 17:35:20 +0600

    19th Alert message

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,19 +15,53 @@ import android.widget.TimePicker;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+private Button btn;
 
+private AlertDialog.Builder alertDialogBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
+        btn = findViewById(R.id.btnID);
+        btn.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+        alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("Title");
+        alertDialogBuilder.setMessage("This is message!");
+
+        alertDialogBuilder.setPositiveButton("Yes",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                        System.out.println("Yes : "+which);
+                    }
+                });
+        alertDialogBuilder.setNegativeButton("No",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                        System.out.println("NO : "+which);
+                    }
+                });
+        alertDialogBuilder.setNeutralButton("Nothing",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                        System.out.println("Neutral : "+which);
+                    }
+                });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
 
     }
+
+
 }
