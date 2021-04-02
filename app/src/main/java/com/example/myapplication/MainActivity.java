92dad050e2c9c796f06d654e353d7671f5575927
mfrfrabbi <mdfazlerabbi.diu@gmail.com>
2021-04-02 17:29:29 +0600

    21th videoView

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,19 +6,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.DialogInterface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.VideoView;
 import android.widget.ZoomControls;
+
+import java.net.URL;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 private ZoomControls zoomControls;
 private ImageView image;
+private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +56,15 @@ private ImageView image;
 
             }
         });
+
+        //video view
+        videoView = findViewById(R.id.videoViewId);
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.video);
+        videoView.setVideoURI(uri);
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        //videoView.start();
+
 
     }
 
