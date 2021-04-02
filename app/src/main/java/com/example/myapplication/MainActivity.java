77d9fe370eb7156c263f07573153a9762a063cd2
mfrfrabbi <mdfazlerabbi.diu@gmail.com>
2021-04-02 16:52:59 +0600

    20th ZoomControl

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,56 +10,51 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.ZoomControls;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-private Button btn;
-
-private AlertDialog.Builder alertDialogBuilder;
+private ZoomControls zoomControls;
+private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn = findViewById(R.id.btnID);
-        btn.setOnClickListener(this);
+        zoomControls = findViewById(R.id.zoomId);
+        image = findViewById(R.id.imageId);
+
+        zoomControls.setOnZoomInClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float x = image.getScaleX();
+                float y = image.getScaleY();
+                image.setScaleX((float)x+1);
+                image.setScaleY((float)y+1);
+
+            }
+        });
+        zoomControls.setOnZoomOutClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float x = image.getScaleX();
+                float y = image.getScaleY();
+                if(x>1 && y>1){
+                    image.setScaleX((float) x-1);
+                    image.setScaleY((float)y-1);
+                }
+
+
+            }
+        });
+
     }
 
     @Override
     public void onClick(View v) {
-        alertDialogBuilder = new AlertDialog.Builder(this);
-        alertDialogBuilder.setTitle("Title");
-        alertDialogBuilder.setMessage("This is message!");
-
-        alertDialogBuilder.setPositiveButton("Yes",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        finish();
-                        System.out.println("Yes : "+which);
-                    }
-                });
-        alertDialogBuilder.setNegativeButton("No",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                        System.out.println("NO : "+which);
-                    }
-                });
-        alertDialogBuilder.setNeutralButton("Nothing",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                        System.out.println("Neutral : "+which);
-                    }
-                });
-
-        AlertDialog alertDialog = alertDialogBuilder.create();
-        alertDialog.show();
 
     }
 
